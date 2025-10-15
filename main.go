30b17ff8977b5ce6ff64a01306e0d47b624aca7f
makrenka <smokrenko79@gmail.com>
2025-10-15 16:50:45 +0300

    math operators

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"math"
 	"math/rand/v2"
+	"strconv"
 )
 
 func main() {
 	random := math.Floor((rand.Float64()*100)*10) / 10
+	var isEven bool
+	if math.Mod(random, 2) == 0 {
+		isEven = true
+	} else {
+		isEven = false
+	}
+
 	fmt.Println("Исходное число: ", random)
-	fmt.Println("Исходное число, увеличенное на 10%: ", math.Round(random*1.1*100000)/100000)
-	fmt.Println("Исходное число является четным: ", math.Mod(random, 2))
+	fmt.Println("Исходное число, увеличенное на 10%: ", strconv.FormatFloat(random*1.1, 'f', 5, 64))
+	fmt.Println("Исходное число является четным: ", isEven)
+	fmt.Println("Предпоследняя цифра целой части исходного числа: ", math.Floor(random/10))
 }

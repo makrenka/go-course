88231b964a1db49ca5fdafe99382b65b43ddffe9
makrenka <smokrenko79@gmail.com>
2025-10-19 21:40:57 +0300

    add generateCompliment

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand/v2"
 )
 
 func main() {
@@ -19,32 +19,51 @@ func main() {
 	// fmt.Println("Исходное число является четным: ", isEven)
 	// fmt.Println("Предпоследняя цифра целой части исходного числа: ", math.Floor(random/10))
 
-	printNumberInfo(0)
+	result := generateCompliment("Serg")
+	fmt.Println(result)
 }
 
-func printNumberInfo(num int) {
-	if math.Abs(float64(num)) != float64(num) {
-		fmt.Printf("Число %d отрицательное.\n", num)
-	} else if num == 0 {
-		fmt.Printf("Число равно %d.\n", num)
-	} else {
-		fmt.Printf("Число %d положительное.\n", num)
+func generateCompliment(name string) string {
+	randomNum := rand.IntN(3)
+	var message string
 
+	switch {
+	case randomNum == 0:
+		message = "Ты великолепен, " + name + "!"
+	case randomNum == 1:
+		message = "У тебя потрясающая улыбка, " + name + "!"
+	case randomNum == 2:
+		message = "Ты вдохновляешь, " + name + "!"
+	default:
+		fmt.Println("Что-то пошло не так")
 	}
 
-	if num%2 != 0 {
-		fmt.Printf("Число %d нечетное.\n", num)
-	} else {
-		fmt.Printf("Число %d четное.\n", num)
-	}
-
-	sqrt := math.Sqrt(float64(num))
-	if math.Abs(float64(num)) == float64(num) && num%2 != 0 {
-		fmt.Printf("Квадратный корень числа %d не является целым числом и равен %.5f.\n", num, sqrt)
-	} else if math.Abs(float64(num)) == float64(num) && num%2 == 0 && num != 0 {
-		fmt.Printf("Квадратный корень числа %d является целым числом и равен %d.\n", num, int(sqrt))
-	}
+	return message
 }
+
+// func printNumberInfo(num int) {
+// 	if math.Abs(float64(num)) != float64(num) {
+// 		fmt.Printf("Число %d отрицательное.\n", num)
+// 	} else if num == 0 {
+// 		fmt.Printf("Число равно %d.\n", num)
+// 	} else {
+// 		fmt.Printf("Число %d положительное.\n", num)
+
+// 	}
+
+// 	if num%2 != 0 {
+// 		fmt.Printf("Число %d нечетное.\n", num)
+// 	} else {
+// 		fmt.Printf("Число %d четное.\n", num)
+// 	}
+
+// 	sqrt := math.Sqrt(float64(num))
+// 	if math.Abs(float64(num)) == float64(num) && num%2 != 0 {
+// 		fmt.Printf("Квадратный корень числа %d не является целым числом и равен %.5f.\n", num, sqrt)
+// 	} else if math.Abs(float64(num)) == float64(num) && num%2 == 0 && num != 0 {
+// 		fmt.Printf("Квадратный корень числа %d является целым числом и равен %d.\n", num, int(sqrt))
+// 	}
+// }
 
 // ---------------------------------------------------------------------------------------
 // На основе полученного значения ИМТ, программа должна определить и вывести соответствующую категорию:

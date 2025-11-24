27bd75a10a687310730a4e66b027f48c1e79a488
makrenka <smokrenko79@gmail.com>
2025-11-24 13:15:10 +0300

    add to workWithSlices

--- lessons/workWithSlices.go
+++ lessons/workWithSlices.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand/v2"
+	"slices"
 )
 
 func CreateSlice(n int) ([]int, error) {
@@ -31,4 +32,65 @@ func FilterSlice(numbers []int) []int {
 	}
 	fmt.Println(res)
 	return res
+}
+
+func MaxSumWithNegative(numbers []int, k int) []int {
+	maxSumSlice := make([]int, k)
+	var maxSum int
+	for i := range numbers {
+		var subSlice []int
+		if i+k > len(numbers) {
+			break
+		}
+		if i+k == len(numbers) {
+			subSlice = numbers[i:]
+		}
+		if i+k < len(numbers) {
+			subSlice = numbers[i : i+k]
+		}
+		var negativeSlice []int
+		sum := 0
+		for _, v := range subSlice {
+			if v < 0 {
+				negativeSlice = append(negativeSlice, v)
+			}
+		}
+		if len(negativeSlice) > 0 {
+			for _, v := range subSlice {
+				sum += v
+			}
+		}
+		if sum > maxSum || i == 0 {
+			maxSum = sum
+			copy(maxSumSlice, subSlice)
+		}
+	}
+	fmt.Println(maxSumSlice)
+	return maxSumSlice
+}
+
+func SortByParity(numbers []int) []int {
+	slices.SortFunc(numbers, compareSliceNums)
+	fmt.Println(numbers)
+	return numbers
+}
+
+func isEvenNum(num int) bool {
+	return num%2 == 0
+}
+
+func compareSliceNums(a, b int) (res int) {
+	if isEvenNum(a) && !isEvenNum(b) {
+		res = -1
+	}
+	if !isEvenNum(a) && isEvenNum(b) {
+		res = 1
+	}
+	if isEvenNum(a) && isEvenNum(b) {
+		res = b - a
+	}
+	if !isEvenNum(a) && !isEvenNum(b) {
+		res = a - b
+	}
+	return
 }
--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	res, _ := lessons.CreateSlice(5)
-	lessons.FilterSlice(res)
+	// res, _ := lessons.CreateSlice(6)
+	lessons.SortByParity([]int{-7, -5, 6, -10, 2, -9})
 }

07a5cb097850254e27f53c212acb9413ab529861
makrenka <smokrenko79@gmail.com>
2025-12-15 12:42:21 +0300

    add handlePaymentSystemErrors

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"go-course/lessons"
+	"errors"
+	"fmt"
+	"go-course/practices"
+	"os"
 )
 
 func main() {
-	lessons.Service.PrintChat(lessons.Service{}, 584937)
+	if err := practices.HandlePayments(); err != nil {
+		// Обработать ошибки, которые могут произойти при вызове HandlePayments
+		var getInsufficientFundsError *practices.InsufficientFundsError
+
+		switch {
+		case errors.Is(err, practices.ErrUnsupportedPaymentMethod):
+			fmt.Println("Ошибка: неподдерживаемый метод платежа.")
+		case errors.As(err, &getInsufficientFundsError):
+			fmt.Printf("Ошибка: недостаточно средств. requested amount: %.2f, maximum allowed amount: %.2f\n",
+				getInsufficientFundsError.RequestedAmount,
+				getInsufficientFundsError.MaxAmount)
+		default:
+			fmt.Printf("Неизвестная ошибка: %s.", err)
+		}
+
+		os.Exit(1)
+	}
+
+	fmt.Println("Платеж успешно обработан!")
 }
---
+++ practices/handlePaymentSystemErrors.go
@@ -0,0 +1,95 @@
+package practices
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+// Заранее подготавливаем некоторые ошибки
+var (
+	ErrUnsupportedPaymentMethod = errors.New("unsupported payment method")
+)
+
+// Ошибка нехватки средств
+type InsufficientFundsError struct {
+	RequestedAmount float64
+	MaxAmount       float64
+}
+
+func (e *InsufficientFundsError) Error() string {
+	return fmt.Sprintf("requested amount: %.2f, maximum allowed amount: %.2f", e.RequestedAmount, e.MaxAmount)
+}
+
+// PaymentProcessor обрабатывает платежи
+type PaymentProcessor struct{}
+
+// NewPaymentProcessor создает новый процессор платежей
+func NewPaymentProcessor() *PaymentProcessor {
+	return &PaymentProcessor{}
+}
+
+// ProcessPayment обрабатывает платеж
+func (pp *PaymentProcessor) ProcessPayment(method string, amount float64) error {
+	if method != "карта" && method != "СБП" {
+		return ErrUnsupportedPaymentMethod
+	}
+
+	// Имитация проверки средств
+	maxAmount := 1000.0
+	if amount > maxAmount {
+		return &InsufficientFundsError{
+			RequestedAmount: amount,
+			MaxAmount:       maxAmount,
+		}
+	}
+
+	return nil
+}
+
+func HandlePayments() error {
+	var method string
+	var amountStr string
+
+	// Спросить метод перевода в консоли
+	fmt.Print("Введите метод перевода (карта/СБП): ")
+	fmt.Scanln(&method)
+
+	// Спросить сумму перевода в консоли
+	fmt.Print("Введите сумму перевода: ")
+	fmt.Scanln(&amountStr)
+
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		return fmt.Errorf("invalid amount: %w", err)
+	}
+
+	pp := NewPaymentProcessor()
+	if err := pp.ProcessPayment(method, amount); err != nil {
+		return fmt.Errorf("process payment: %w", err)
+	}
+
+	return nil
+}
+
+// func main() {
+// 	if err := practices.HandlePayments(); err != nil {
+// 		// Обработать ошибки, которые могут произойти при вызове HandlePayments
+// 		var getInsufficientFundsError *practices.InsufficientFundsError
+
+// 		switch {
+// 		case errors.Is(err, practices.ErrUnsupportedPaymentMethod):
+// 			fmt.Println("Ошибка: неподдерживаемый метод платежа.")
+// 		case errors.As(err, &getInsufficientFundsError):
+// 			fmt.Printf("Ошибка: недостаточно средств. requested amount: %.2f, maximum allowed amount: %.2f\n",
+// 				getInsufficientFundsError.RequestedAmount,
+// 				getInsufficientFundsError.MaxAmount)
+// 		default:
+// 			fmt.Printf("Неизвестная ошибка: %s.", err)
+// 		}
+
+// 		os.Exit(1)
+// 	}
+
+// 	fmt.Println("Платеж успешно обработан!")
+// }
